2b3d8d23deec6a5a7065ad86bfd37d54f1373c45
Dima <dimazelinskiy1@gmail.com>
2024-10-11 14:37:15 +0200

    Menu added + defined all necessary things

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,23 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    private Scanner readLine = new Scanner(System.in);
+    public int menu(){
+        System.out.println(" -- Calculator -- ");
+        System.out.println("Choose: "
+                + "\n0 - Exit\n1 - Add\n2 - Subtract\n3 - Multiply" +
+                "\n4 - Divide\n5 - Sqrt\n6 - Power");
+        System.out.println("Please, insert chosen option: ");
+        int choice = -1;
+        choice = readLine.nextInt();
+        return choice;
+    }
+    public static void main(String[] args) {
+
+
+    }
 }
+
+
+//add subtract multiply
+//divide sqrt power
